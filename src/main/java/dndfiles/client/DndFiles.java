aa697a6812ef66e29c2f7648221418de35a96a1e
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-09-21 08:55:03 +0200

    Compact code style

--- src/main/java/dndfiles/client/DndFiles.java
+++ src/main/java/dndfiles/client/DndFiles.java
@@ -7,53 +7,38 @@ import static elemental2.Global.document;
 
 // http://www.html5rocks.com/en/tutorials/file/dndfiles/
 public class DndFiles implements EntryPoint {
-    private HTMLOutputElement list;
+    private HTMLOutputElement out;
 
     @Override
     public void onModuleLoad() {
         HTMLInputElement file = (HTMLInputElement) Global.document.createElement("input");
-        file.type = "file";
-        file.id = "files";
-        file.name = "files[]";
-        file.multiple = true;
+        file.type = "file"; file.id = "files"; file.name = "files[]"; file.multiple = true;
 
-        list = (HTMLOutputElement) document.createElement("output");
-        list.id = "list";
+        out = (HTMLOutputElement) document.createElement("output");
 
         document.body.appendChild(file);
-        document.body.appendChild(list);
+        document.body.appendChild(out);
 
-        file.addEventListener("change", evt -> {
-            show(((HTMLInputElement) evt.target).files);
-        }, false);
-
-        document.body.addEventListener("dragover", evt -> {
-            evt.stopPropagation();
-            evt.preventDefault();
-            ((DragEvent) evt).dataTransfer.dropEffect = "copy";
-        }, false);
-        document.body.addEventListener("drop", evt -> {
-            evt.stopPropagation();
-            evt.preventDefault();
-            show(((DragEvent) evt).dataTransfer.files);
-        }, false);
+        file.addEventListener("change",
+                e -> { show(((HTMLInputElement) e.target).files); }, false);
+        document.body.addEventListener("dragover",
+                e -> { consume(e); ((DragEvent) e).dataTransfer.dropEffect = "copy"; }, false);
+        document.body.addEventListener("drop",
+                e -> { consume(e); show(((DragEvent) e).dataTransfer.files); }, false);
     }
 
     private void show(FileList files) {
         for (int i = 0; i < files.length; i++) {
             File f = files.item(i);
 
-            final String name = document.createTextNode(f.name).wholeText;
-            final String type = document.createTextNode(f.type).wholeText;
-            list.innerHTML += "<li><strong>" + name + "</strong> (" + type + ") - " + f.size + " bytes, "
-                    + "last modified: " + f.lastModifiedDate;
+            out.innerHTML += "<li><strong>" + f.name + "</strong> (" + f.type + ") - "
+                    + f.size + " bytes, last modified: " + f.lastModifiedDate;
 
             FileReader reader = new FileReader();
-            reader.onprogress = loaded -> {
-                list.innerHTML += "<li>loading... " + loaded.loaded + "/" + loaded.total;
-                return null;
-            };
+            reader.onprogress = e -> { out.innerHTML += "<li>"+ e.loaded / e.total; return null; };
             reader.readAsBinaryString(f);
         }
     }
+
+    private static void consume(Event e) { e.stopPropagation(); e.preventDefault(); }
 }
