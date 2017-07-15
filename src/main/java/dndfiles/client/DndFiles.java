4cf37cb7c5cbda73c49582bc3339f4a94d7b0d15
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2017-07-15 19:49:34 +0200

    Upgrade to RxElemento (elemento + rxgwt)

--- src/main/java/dndfiles/client/DndFiles.java
+++ src/main/java/dndfiles/client/DndFiles.java
@@ -4,42 +4,55 @@ import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.*;
 import org.jboss.gwt.elemento.core.Elements;
 import org.jboss.gwt.elemento.core.InputType;
+import rx.Observable;
 
-import java.util.function.Consumer;
-
+import static com.intendia.rxgwt.elemento.RxElemento.fromEvent;
 import static elemental2.dom.DomGlobal.document;
+import static org.jboss.gwt.elemento.core.Elements.*;
 import static org.jboss.gwt.elemento.core.EventType.*;
 
 // http://www.html5rocks.com/en/tutorials/file/dndfiles/
-public class DndFiles implements EntryPoint { //@formatter:off
+public class DndFiles implements EntryPoint {
 
-    @Override public void onModuleLoad() {
+    @Override
+    public void onModuleLoad() {
         // create UI
-        HTMLInputElement file = Elements.input(InputType.file).id("files")
-                .apply(el -> { el.name = "files[]"; el.multiple = true; }).asElement();
+        HTMLInputElement file = Elements.input(InputType.file).asElement();
         document.body.appendChild(file);
 
         HTMLOutputElement out = Elements.output().asElement();
         document.body.appendChild(out);
 
         // bind UI
-        Consumer<FileList> show = files -> {
+
+        Observable<FileList> input$ = fromEvent(file, change)
+                .map(e -> file.files);
+        Observable<FileList> drop$ = fromEvent(document.body, drop)
+                .map(e -> consume(e).dataTransfer.files);
+        Observable.merge(input$, drop$).subscribe(files -> {
             for (int i = 0; i < files.length; i++) {
                 File f = files.item(i);
+                out.appendChild(li()
+                        .add(strong().textContent(f.name))
+                        .add(span().textContent(" (" + f.type + ") - " + f.size + "bytes"))
+                        .asElement());
 
-                out.innerHTML += "<li><strong>" + f.name + "</strong> (" + f.type + ") - "
-                        + f.size + " bytes, last modified: " + f.lastModifiedDate;
+                HTMLLIElement li = li().asElement();
+                out.appendChild(li);
 
                 FileReader reader = new FileReader();
-                reader.onprogress = e -> { out.innerHTML += "<li>"+ e.loaded / e.total; return null; };
+                reader.onprogress = e -> li.textContent = (e.loaded / e.total) + " progress";
+                reader.onload = e -> li.textContent = reader.result.asString().substring(0, 100);
                 reader.readAsBinaryString(f);
             }
-        } ;
-        bind(file, change, e -> { show.accept(((HTMLInputElement) e.target).files); });
-        bind(document.body, dragover, e -> { consume(e); e.dataTransfer.dropEffect = "copy"; });
-        bind(document.body, drop, e -> { consume(e); show.accept(e.dataTransfer.files); });
+        });
+        fromEvent(document.body, dragover)
+                .subscribe(e -> consume(e).dataTransfer.dropEffect = "copy");
     }
 
-
-    private static void consume(Event e) { e.stopPropagation(); e.preventDefault(); }
+    private static <E extends Event> E consume(E e) {
+        e.stopPropagation();
+        e.preventDefault();
+        return e;
+    }
 }
